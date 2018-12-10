fa4643c6c7a826e11e45aa9bbf89799aa16b0ba2
Tianyi Ma <31545688+mtybilly@users.noreply.github.com>
2018-12-09 17:32:29 -0700

    Add files via upload

--- Hannoi_Algorithm.py
+++ Hannoi_Algorithm.py
@@ -3,15 +3,18 @@ import numpy as np
 solution = []
 status = []
 store= []
-def display():
+def display(index):
+	print("Initial status:", store[index])
 	for i in range(index,len(solution)):
 		string = "Move disk " + str(solution[i][0]) + " from " + str(solution[i][1]) + " to " + str(solution[i][2])
 		print(string)
-		print(store[i])
+		print(store[i+1])
 def updataStatus():
 	global status
 	global store
 	index = 0
+	store[index] = status
+	index += 1 
 	for i in range(len(solution)):
 	  plate = solution[i][0] 
 	  pre_state = solution[i][1] - 1
@@ -23,7 +26,7 @@ def updataStatus():
 	  	for m in range(3):
 	  		for j in range(len(status[m])):
 	  			store[index][m].append(status[m][j])
-	  	index += 1	
+	  	index += 1	  	
 def looking_For_Step(temp):
 	temp[0].sort()
 	temp[1].sort()
@@ -32,15 +35,11 @@ def looking_For_Step(temp):
 	for i in range(len(store)):
 		if temp[0] in store[i] and temp[1] in store[i] and temp[2] in store[i] and check == -1:
 			check = i 	
-	if check >= 1:		
-		return check-1
-	else: return check 	
+	return check	
 def updataSolution(n,A,C):
     global solution
     solution.append((n, A, C))
 def hannoi(n, A, B, C):
-	global solution
-	global numSolution 
 	if n == 0 :
 		return
 	else:
@@ -57,7 +56,7 @@ def main():
    store =  [ [ [], [], [] ] for i in range(n*n*n)]
    hannoi(n,1,2,3)  # TM: calculate the hannoi, the answer is in solution ( global matrix)
    updataStatus()   # TM: converte solution to status ( global step * 3 * n )
-   index = looking_For_Step() # TM: looking for given status
-   display()
+   index = looking_For_Step([[1,2,3,4,5],[],[]]) # TM: looking for given status
+   display(index)
 if __name__ == '__main__':
 	main()
