7d453e54b6992faa040e5ed476fde87de02538e0
ZormeneKinanee <33584010+ZormeneKinanee@users.noreply.github.com>
2018-12-14 15:41:00 -0700

    color->peg->block association
    should connect colors to pegs, boundaries for detecting color may be off slightly

--- detect_color.py
+++ detect_color.py
@@ -3,53 +3,55 @@ import numpy as np
 import argparse
 import cv2
 
-def detectColor(color, distance):
-    
-    while True:
-	ap = argparse.ArgumentParser()
-	ap.add_argument("-i", "--image", help = "path to the image")
-	args = vars(ap.parse_args())
 
-	image = cv2.imread(args["image"])
+def detectColor():
+	red=[36,30,100],[43,57,192] 
+	green=[59,106,24],[113,204,46]
+	purple=[131,52,108],[173,68,142]
+	yellow=[63,208,244],[111,220,247]
+	blue=[111,61,35],[219,152,52]
 
-	boundaries = [(red),(green),(purple),(yellow),(blue)]
+    #initial states of
 
-    # create NumPy arrays from the boundaries
-	for (lower, upper) in boundaries:
-		lower = np.array(lower, dtype = "uint8")
-		upper = np.array(upper, dtype = "uint8")
+	while True:
+		ap = argparse.ArgumentParser()
+    	ap.add_argument("-i", "--image", help = "path to the image")
+    	args = vars(ap.parse_args())
+    	image = cv2.imread(args["image"])
+    	boundaries = [(green),(blue),(red),(yellow),(purple)]
 
-		mask = cv2.inRange(image, lower, upper)
-		output = cv2.bitwise_and(image, image, mask = mask)
+    	for (lower, upper) in boundaries:
+    		lower = np.array(lower, dtype = "uint8")
+    		upper = np.array(upper, dtype = "uint8")
 
-		# find the colors within the specified boundaries and apply
-		# the mask
-		returnColor=cv2.imshow("images", np.hstack([image,output]))
-		if cv2.waitKey(0) & 0xFF == ord('q'):break
+    		mask = cv2.inRange(image, lower, upper)
+    		output = cv2.bitwise_and(image, image, mask = mask)
 
-		return returnColor
+    		pegs=[1,2,3]
+
+    		if output==boundaries[0]:
+    			greenBlock=1
+    			peg=pegs[0]
+    		elif output==boundaries[1]:
+    			blueBlock=2
+    			peg=pegs[0]
+    		elif output==boundaries[2]:
+    			redBlock=3
+    			peg=pegs[0]
+    		elif output==boundaries[3]:
+    			yellowBlock=4
+    			peg=pegs[0]
+    		elif output==pboundaries[4]:
+    			purpleBlock=5
+    			peg=pegs[0]
+
+    		return peg
 
 
-red=[36,30,100],[43,57,192] 
-green=[59,106,24],[113,204,46]
-purple=[131,52,108],[173,68,142]
-yellow=[63,208,244],[111,220,247]
-blue=[111,61,35],[219,152,52]
+    		# # pegIdentifier=cv2.imshow("images", np.hstack([image,output]))
 
-distance=3
-detectColor(red, distance)
-
-
-# cv2.imshow("images", np.hstack([image, output]))
-
-
-
-
-# colorRed=boundaries[0]
-# colorGreen=boundaries[1]
-# colorPurple=boundaries[2]
-# colorYellow=boundaries[3]
-# colorBlue=boundaries[4]
+    		# if cv2.waitKey(0) & 0xFF == ord('q'):
+    		# 	break
 
 
 
