abda7f2ecccd4313d828a895148b3d4983bb4967
ZormeneKinanee <33584010+ZormeneKinanee@users.noreply.github.com>
2018-12-14 14:22:13 -0700

    Add files via upload
    run through command line using: python detect_color.py --image map.jpg

--- detect_color.py
+++ detect_color.py
@@ -1,51 +1,67 @@
-# USAGE
 # python detect_color.py --image pokemon_games.png
-
-# import the necessary packages
 import numpy as np
 import argparse
 import cv2
 
-# construct the argument parse and parse the arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--image", help = "path to the image")
-args = vars(ap.parse_args())
+def detectColor(color, distance):
+    
+    while True:
+	ap = argparse.ArgumentParser()
+	ap.add_argument("-i", "--image", help = "path to the image")
+	args = vars(ap.parse_args())
 
-# load the image
-image = cv2.imread(args["image"])
+	image = cv2.imread(args["image"])
 
-# red=[8,255,255],[8,255,255]
-yellow=[0,255,255],[100,255,255] #RGB. BGR #works 
-green=[0,150,0],[255,255,10] #works
-green=[70,248,71],[255,255,10]
-blue=[124,158,78],[255,255,138]
-#BGR
-# NumPy is in reverse order BGR [lower bound] [upper bound]
+	boundaries = [(red),(green),(purple),(yellow),(blue)]
+
+    # create NumPy arrays from the boundaries
+	for (lower, upper) in boundaries:
+		lower = np.array(lower, dtype = "uint8")
+		upper = np.array(upper, dtype = "uint8")
+
+		mask = cv2.inRange(image, lower, upper)
+		output = cv2.bitwise_and(image, image, mask = mask)
+
+		# find the colors within the specified boundaries and apply
+		# the mask
+		returnColor=cv2.imshow("images", np.hstack([image,output]))
+		if cv2.waitKey(0) & 0xFF == ord('q'):break
+
+		return returnColor
 
 
-# Here, we are saying that all pixels in our image that have a R >= 100, B >= 15, and G >= 17 along with R <= 
-# define the list of boundaries
-boundaries = [
-       # (yellow),
-       (green)
-	# (red),
-	
-	# ([25, 146, 190], [62, 174, 250]),
-	# ([103, 86, 65], [145, 133, 128])
-]
+red=[36,30,100],[43,57,192] 
+green=[59,106,24],[113,204,46]
+purple=[131,52,108],[173,68,142]
+yellow=[63,208,244],[111,220,247]
+blue=[111,61,35],[219,152,52]
 
-# loop over the boundaries
-for (lower, upper) in boundaries:
-	# create NumPy arrays from the boundaries
-	lower = np.array(lower, dtype = "uint8")
-	upper = np.array(upper, dtype = "uint8")
+distance=3
+detectColor(red, distance)
 
-	# find the colors within the specified boundaries and apply
-	# the mask
-	mask = cv2.inRange(image, lower, upper)
-	output = cv2.bitwise_and(image, image, mask = mask)
 
-	# show the images
-	cv2.imshow("images", np.hstack([image, output]))
-	if cv2.waitKey(0) & 0xFF == ord('q'):
-		break
+# cv2.imshow("images", np.hstack([image, output]))
+
+
+
+
+# colorRed=boundaries[0]
+# colorGreen=boundaries[1]
+# colorPurple=boundaries[2]
+# colorYellow=boundaries[3]
+# colorBlue=boundaries[4]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
